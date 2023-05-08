028f667f3236310796ed1466642cda55362c3619
Martin Metsoja <suvaline.vana@gmail.com>
2023-05-08 13:11:49 +0300

    deploy

---
+++ js/about.d9b2c9e5.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkpipecraft2_web"]=self["webpackChunkpipecraft2_web"]||[]).push([[443],{7750:function(t,e,n){n.r(e),n.d(e,{default:function(){return c}});var u=function(){var t=this;t._self._c;return t._m(0)},s=[function(){var t=this,e=t._self._c;return e("div",{staticClass:"about"},[e("h1",[t._v("This is an about page")])])}],a=n(1001),r={},i=(0,a.Z)(r,u,s,!1,null,null,null),c=i.exports}}]);
+//# sourceMappingURL=about.d9b2c9e5.js.map
